5dc358062458ebe1da034c4dd7bcdc7286697443
Hayal Cezayirli <hcezayirli@student.tgm.ac.at>
2026-09-22 12:51:13 +0200

    Add GewinnModel structure

---
+++ src/GewinnModel.java
@@ -0,0 +1,27 @@
+public class GewinnModel {
+
+    private int gesamtPunkte;
+    private int spielerZahl;
+    private int computerZahl;
+    private int rundenErgebnis;
+
+    public GewinnModel() {
+        gesamtPunkte = 30;
+    }
+
+    public int getGesamtPunkte() {
+        return gesamtPunkte;
+    }
+
+    public int getSpielerZahl() {
+        return spielerZahl;
+    }
+
+    public int getComputerZahl() {
+        return computerZahl;
+    }
+
+    public int getRundenErgebnis() {
+        return rundenErgebnis;
+    }
+}
