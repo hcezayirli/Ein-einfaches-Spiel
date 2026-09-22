3cfbcb02ef30ef1f0879eadd7059a5f8fdc7630c
Hayal Cezayirli <hcezayirli@student.tgm.ac.at>
2026-09-22 12:54:26 +0200

    Add round and score calculation

--- src/GewinnModel.java
+++ src/GewinnModel.java
@@ -28,4 +28,28 @@ public class GewinnModel {
     public void berechneComputerZahl() {
         computerZahl = (int) (Math.random() * 9) + 1;
     }
+
+    public void berechneRunde(int spielerZahl) {
+
+        // Ungültige Zahlen werden ignoriert
+        if (spielerZahl < 1 || spielerZahl > 9) {
+            return;
+        }
+
+        this.spielerZahl = spielerZahl;
+
+        berechneComputerZahl();
+
+        int unterschied = Math.abs(spielerZahl - computerZahl);
+
+        if (unterschied == 0) {
+            rundenErgebnis = 20;
+        } else if (unterschied == 1) {
+            rundenErgebnis = 5;
+        } else {
+            rundenErgebnis = -10;
+        }
+
+        gesamtPunkte += rundenErgebnis;
+    }
 }
